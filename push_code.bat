60f6665a4219629bfa4861467a889b4ded4a3c66
spservicesgroupinc-blip <spservicesgroupinc@gmail.com>
2026-01-30 05:58:16 -0500

    Add files via upload

---
+++ push_code.bat
@@ -0,0 +1,18 @@
+@echo off
+echo ========================================================
+echo               PUSHING CODE TO GITHUB
+echo ========================================================
+echo.
+echo This script will send your code to:
+echo https://github.com/spservicesgroupinc-blip/antigravity.git
+echo.
+echo If a window pops up asking you to sign in, please do so.
+echo.
+echo Running: git push -u origin main
+echo.
+git push -u origin main
+echo.
+echo ========================================================
+echo               DONE
+echo ========================================================
+pause
